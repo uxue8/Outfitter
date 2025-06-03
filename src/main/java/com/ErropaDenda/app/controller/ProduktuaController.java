548d6749df0e58ad6ea583f3613834cc99408f9c
Usuario <uxuitasantos@gmail.com>
2025-06-03 15:25:44 +0200

    clases borradas

--- src/main/java/com/ErropaDenda/app/controller/ProduktuaController.java
+++ src/main/java/com/ErropaDenda/app/controller/ProduktuaController.java
@@ -48,73 +48,7 @@ public class ProduktuaController {
 
 	@PersistenceContext
 	private EntityManager entityManager;
-	/*
-	 * @GetMapping("/admin/produktuak") public String ikusiProduktuak(Model model) {
-	 * Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	 * String email = auth.getName();
-	 * 
-	 * Erabiltzailea erabiltzailea = erabRepository.findByEmail(email)
-	 * .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
-	 * model.addAttribute("erabiltzailea", erabiltzailea);
-	 * 
-	 * String rol = auth.getAuthorities().stream().map(authority ->
-	 * authority.getAuthority()).findFirst().orElse(null);
-	 * model.addAttribute("rola", rol); Iterable<Erropa> prod = prodRepo.findAll();
-	 * if (prodRepo.findAll().isEmpty()) { model.addAttribute("produktuak", null); }
-	 * else {
-	 * 
-	 * model.addAttribute("produktuak", prod); }
-	 * 
-	 * return "taulak/produktuaTaula"; }
-	 * 
-	 * @GetMapping("/admin/gehitu") public String showAddProductForm(Model model) {
-	 * model.addAttribute("produktua", new Erropa()); return
-	 * "/formularioa/produktuaForm";
-	 * 
-	 * }
-	 * 
-	 * 
-	 * 
-	 * @GetMapping("/admin/ezabatu/{id}") public String
-	 * eliminarProducto(@PathVariable Long id) { Optional<Erropa> produktoa =
-	 * prodRepo.findById(id);
-	 * 
-	 * if (produktoa.isPresent()) { Erropa produktua = produktoa.get();
-	 * 
-	 * List<Cesta> cestas = cestaRepository.findByProduktuak(produktua); for (Cesta
-	 * cesta : cestas) { cesta.getProduktuak().remove(produktua);
-	 * 
-	 * cestaRepository.save(cesta); } List<Erabiltzailea> erab =
-	 * erabRepository.findByErropak(produktua); for (Erabiltzailea erabiltzailea :
-	 * erab) { erabiltzailea.getErropak().remove(produktua);
-	 * erabRepository.save(erabiltzailea); } prodRepo.delete(produktua); } else {
-	 * System.out.println("Producto no encontrado con id: " + id); }
-	 * 
-	 * return "redirect:/produktua/admin/produktuak"; }
-	 */
-	/*
-	 * @GetMapping("/admin/editatu/{id}") public String aldatuProd(@PathVariable
-	 * Long id, Model model) { Optional<Erropa> prod = prodRepo.findById(id); if
-	 * (prod.isPresent()) { model.addAttribute("produktua", prod.get()); } else {
-	 * return "error/notFound"; } return "formularioa/aldatuProd"; }
-	 * 
-	 * @PostMapping("/admin/editatu/{id}") public String
-	 * aldatuProduktua(@ModelAttribute("produktua") Erropa producto,
-	 * 
-	 * @RequestParam("file") MultipartFile irudia, Model model) { try { if
-	 * (!irudia.isEmpty()) { Path uploadDir =
-	 * Paths.get("src/main/resources/static/uploads"); String rutaAbsoluta =
-	 * uploadDir.toFile().getAbsolutePath(); byte[] bytesImg = irudia.getBytes();
-	 * Path rutaCompleta = Paths.get(rutaAbsoluta + "/" +
-	 * irudia.getOriginalFilename()); Files.write(rutaCompleta, bytesImg);
-	 * producto.setIrudiaUrl(irudia.getOriginalFilename()); }
-	 * 
-	 * prodRepo.save(producto);
-	 * 
-	 * return "redirect:/produktua/admin/produktuak"; } catch (IOException e) {
-	 * e.printStackTrace(); model.addAttribute("errorMessage", e.getMessage());
-	 * return "error"; } }
-	 */
+	
 
 	@GetMapping("/gehitu")
 	public String showAddProductForm(Model model) {
@@ -235,39 +169,11 @@ public class ProduktuaController {
 		return "taulak/outfit";
 	}
 
-	
-	/*
-	 * @GetMapping("/produktuak/erosi/{id}") public String
-	 * produktuaErosiUser(@PathVariable Long id, Model model) { Authentication auth
-	 * = SecurityContextHolder.getContext().getAuthentication(); String email =
-	 * auth.getName();
-	 * 
-	 * Erabiltzailea erabiltzailea = erabRepository.findByEmail(email)
-	 * .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
-	 * model.addAttribute("erabiltzailea", erabiltzailea);
-	 * 
-	 * Optional<Cesta> cestaGuztiak =
-	 * cestaRepository.findByErabiltzailea(erabiltzailea);
-	 * 
-	 * Cesta cesta = cestaGuztiak.get();
-	 * 
-	 * if (cesta.getProduktuak() == null) { cesta.setProduktuak(new HashSet<>()); }
-	 * Optional<Erropa> producto = prodRepo.findById(id);
-	 * cesta.getProduktuak().add(producto.get()); cestaRepository.save(cesta);
-	 * erabiltzailea.getErropak().add(producto.get());
-	 * erabRepository.save(erabiltzailea); //para saber si esta comprando en gizona
-	 * o emakumea
-	 * 
-	 * return "redirect:/produktua/produktuak";
-	 * 
-	 * 
-	 * }
-	 */
 
 	private static final Map<String, String> categoriaGeneralPorTipo = Map.of("camiseta", "parteSuperior", "sudadera",
 			"parteSuperior", "chaqueta", "parteSuperior", "pantalon", "parteInferior", "falda", "parteInferior",
 			"zapatilla", "calzado", "camisa", "parteSuperior", "bolso", "accesorio", "gorra", "accesorio"
-	// y así sucesivamente
+	
 	);
 
 }
